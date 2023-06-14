63bae3314467515233cfb30e21c2e55671bdcc57
AleFed <alessandro.fedele@localhost.com>
2023-06-14 12:04:16 +0200

    SegfaultInitializeAutostrada
    try not null, AUTOSTRADA null, discover why

--- Codice/program.c
+++ Codice/program.c
@@ -22,7 +22,8 @@ typedef struct
 route * InitializeAUTOSTRADA(int kms1/*,rbhead * autos*/){
     route * r = (route *) malloc(sizeof(route));
     r->len = 4;
-    r-> AUTOSTRADA = (stazione **) malloc(sizeof(stazione*)*4);
+    stazione ** try = (stazione **) malloc(sizeof(stazione*)*4);
+    r->AUTOSTRADA = try;
     r-> AUTOSTRADA[0] ->kms = kms1;
     r->AUTOSTRADA[0]->next = NULL;
     r->AUTOSTRADA[0]->prev = NULL;
@@ -30,27 +31,39 @@ route * InitializeAUTOSTRADA(int kms1/*,rbhead * autos*/){
     r->lastindex = 0;
     return r;
 }
+void fixmax(route*r){
+    int lastcell = (int)(log2(r->lastindex + 1));
+    stazione* curr = r->AUTOSTRADA[lastcell];
+    while (curr->next != NULL)
+        curr = curr->next;
+    r->AUTOSTRADA[0]->prev = curr;
+}
+stazione * min(route * r){
+    return r->AUTOSTRADA[0];
+}
+stazione * max(route * r){
+    return r->AUTOSTRADA[0]->prev;
+}
 stazione * initializestazione(int km/*,rbhead * autos*/){
-    stazione * s = (stazione * s) malloc(sizeof(stazione));
+    stazione * s = (stazione *) malloc(sizeof(stazione));
     s->kms = km;
     //s->vetture = autos;
     return s; 
 }
-int log2(int n){
-    return int(log(n)/log(2));
-}
 void Check(route * r){
     if(r->len - r-> lastindex < 2){
-        r->AUTOSTRADA = (stazione **) realloc(sizeof(stazione*), r->len * 2); //raddoppio lunghezza vettore.
+        stazione ** new = (stazione **)realloc(r->AUTOSTRADA,sizeof(stazione*)* (r->len * 2)); //raddoppio lunghezza vettore.
+        free(r->AUTOSTRADA);
+        r->AUTOSTRADA = new;
         int i,j;
-        int cells = log2(r->lastindex + 1);
+        int cells = (int)(log2(r->lastindex + 1));
         for(i = 1; i < cells-1; i++) //elemento i-esimo array deve puntare a elemento i posti dopo.
             for(j = 0; j < i; j++) //n-1+1 elementi in [0], [1] si sposta di 1 in avanti. E così via...
                 r->AUTOSTRADA[i] = r->AUTOSTRADA[i]->next;
         r->len *= 2;
     }
 }
-stazione * binarysearch(route r, int km, int start, int stop){
+int binarysearch(route * r, int km, int start, int stop){
     int mid = (int)(ceil((start+stop)/2)); 
     if(km == r->AUTOSTRADA[mid]->kms) //caso ottimo, trovata.
         return (int)(pow(2,mid));
@@ -59,11 +72,11 @@ stazione * binarysearch(route r, int km, int start, int stop){
         if(km > r->AUTOSTRADA[mid+1]->kms) //[mid] è molto lontano da elemento.
             return binarysearch(r,km,mid,stop);
         else if(km == r->AUTOSTRADA[mid+1]->kms)
-            return (int)(pow(2,mid+1)); //int per pow?
+            return (int)(pow(2,mid+1)); 
         else //è tra [mid] e [mid+1]
         {
             int i;
-            int nodes = log2(r->lastindex + 1);
+            int nodes = (int)(log2(r->lastindex + 1));
             stazione * curr = r->AUTOSTRADA[mid];
             for(i = 0; i < nodes; i++)
             {
@@ -71,7 +84,7 @@ stazione * binarysearch(route r, int km, int start, int stop){
                     return (int)(pow(2,mid)) + i;
                 curr = curr ->next;
             }
-            return NULL; //non c'è
+            return -1; //non c'è
         }
     }
     else if(km < r->AUTOSTRADA[mid]->kms) //prima parte.
@@ -83,7 +96,7 @@ stazione * binarysearch(route r, int km, int start, int stop){
         else //in profondità
         {
             int i;
-            int nodes = log2(r->lastindex + 1);
+            int nodes = (int)(log2(r->lastindex + 1));
             stazione * curr = r->AUTOSTRADA[mid-1];
             for(i = 0; i < nodes; i++)
             {
@@ -91,11 +104,11 @@ stazione * binarysearch(route r, int km, int start, int stop){
                     return (int)(pow(2,mid)) + i;
                 curr = curr ->next;
             }
-            return NULL; //non c'è
+            return -1; //non c'è
         }
     }
 }
-void insertion(route r, int km, int index){
+void insertion(route * r, int km, int index){
     int i;
     stazione * curr = r->AUTOSTRADA[index];
     while(curr -> kms <= km && curr -> next != NULL){
@@ -104,20 +117,21 @@ void insertion(route r, int km, int index){
             return;
         }
         else
-        {
             curr = curr ->next;
-        }
     }
+    curr = curr ->prev;//torno ad ultimo riferimento più piccolo "A" e metto tra A e B.
     stazione * ref = curr -> next; //C <- B
     curr -> next = initializestazione(km);//B<-new
     curr ->next->next = ref;//B->next = C;
     curr ->next->prev = curr;//B->prev = A; mentre C->prev ora è A e C->next resta D;
     if(ref != NULL) ref ->prev = curr->next;//C->prev = B.
     r->lastindex++;
+    if(r->AUTOSTRADA[(int)(log2(r->lastindex + 1))]->kms < km) //potrei aver inserito nuovo max.
+        fixmax(r);
     Check(r);
 }
-void inserimento(route r, int km){
-    int stop = log2(r->lastindex+1);
+void inserimento(route * r, int km){
+    int stop = (int)(log2(r->lastindex+1));
     int mid; 
     int start = 0;
     if(r->AUTOSTRADA[stop]->kms < km){
@@ -148,13 +162,30 @@ void inserimento(route r, int km){
     }
 }
 
-void cancella(route r, int km){
-    int index = binarysearch(r,km,0,log2(r->lastindex + 1));
-    if(index == NULL)
+int cercaindice(route* r, int km){
+    return binarysearch(r,km, 0, (int)(log2(r->lastindex + 1)));
+}
+stazione * cerca(route* r, int km){
+    int index = cercaindice(r,km);
+    if(index == -1)
+        return NULL;
+    int cell = (int)(log2(index));
+    int prof = index - (int)(pow(2,cell));
+    int i;
+    stazione * curr = r->AUTOSTRADA[cell];
+    for(i = 0; i <= prof; i++)
+        curr = curr -> next;
+    return curr;
+}
+
+void cancella(route * r, int km){
+    int index = binarysearch(r,km,0,(int)(log2(r->lastindex + 1)));
+    if(index == -1)
     {
+        printf("\n Non c'è.");
         return;
     }
-    int cell = log2(index);
+    int cell = (int)(log2(index));
     stazione * s = r->AUTOSTRADA[cell];
     while (s->next->kms < km)
         s = s->next;
@@ -163,8 +194,23 @@ void cancella(route r, int km){
     A->next = B;
     if(B!=NULL) B->prev = A;
     free(s);
-    //printf("\n Cancellato"); 
+    printf("\n Cancellato"); 
 }
+
+int main(){
+    route * sixtysix = InitializeAUTOSTRADA(100);
+    inserimento(sixtysix, 66);
+    inserimento(sixtysix, 100000);
+    inserimento(sixtysix, 2);
+    inserimento(sixtysix, 200);
+    cancella(sixtysix, 100000);
+    stazione * s = cerca(sixtysix,2);
+    printf("\n %d", s->kms);
+    inserimento(sixtysix, 200);
+    cancella(sixtysix, 3);
+}
+
+
 //inserimento, come binary search, ma nel for vedi while(curr -> kms < km), poi inserisci.
 //per cancella simile.
 
